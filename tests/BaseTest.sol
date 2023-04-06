5ae403111734a1fac8f2718d356d836df0b7ddbf
Rustem Kurmaev <rustboyar@gmail.com>
2023-04-06 15:29:17 +0200

    Enhance proposal test

--- tests/BaseTest.sol
+++ tests/BaseTest.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import {Test} from 'forge-std/Test.sol';
+import {IERC20} from 'solidity-utils/contracts/oz-common/interfaces/IERC20.sol';
 import {ProxyHelpers} from 'aave-helpers/ProxyHelpers.sol';
 import {TestWithExecutor} from 'aave-helpers/GovHelpers.sol';
 import {ICollector} from '../src/interfaces/ICollector.sol';
@@ -16,18 +17,21 @@ abstract contract BaseTest is TestWithExecutor {
   address internal _proxyAdmin;
   address internal _newFundsAdmin;
   uint256 internal _streamId;
+  IERC20 internal _token;
 
   function _setUp(
     address collectorProxy,
     address proxyAdmin,
     address newFundsAdmin,
     uint256 streamId,
-    address executor
+    address executor,
+    IERC20 token
   ) public {
     _collectorProxy = collectorProxy;
     _proxyAdmin = proxyAdmin;
     _newFundsAdmin = newFundsAdmin;
     _streamId = streamId;
+    _token = token;
 
     _collectorImpl = address(new Collector());
 
@@ -70,10 +74,26 @@ abstract contract BaseTest is TestWithExecutor {
     assertEq(collector.getFundsAdmin(), _newFundsAdmin);
 
     // check that funds admin is not the proxy admin
-    vm.prank(_proxyAdmin);
+    vm.startPrank(_proxyAdmin);
 
-    address newAdmin = IInitializableAdminUpgradeabilityProxy(_collectorProxy).admin();
+    IInitializableAdminUpgradeabilityProxy proxy = IInitializableAdminUpgradeabilityProxy(
+      _collectorProxy
+    );
+
+    address newAdmin = proxy.admin();
     assertEq(newAdmin, _proxyAdmin);
+
+    // change admin to someone else
+    address anotherAdmin = vm.addr(0xA11BBB);
+    proxy.changeAdmin(anotherAdmin);
+
+    vm.stopPrank();
+
+    // check that new proxy admin is in charge
+    vm.startPrank(anotherAdmin);
+    assertEq(proxy.admin(), anotherAdmin);
+
+    vm.stopPrank();
 
     // check that stream id is set or not modified
     uint256 newStreamId = collector.getNextStreamId();
@@ -82,5 +102,28 @@ abstract contract BaseTest is TestWithExecutor {
     } else {
       assertEq(newStreamId, currentStreamId);
     }
+
+    address recipient = vm.addr(0xA11CEB);
+    address anotherFundsAdmin = vm.addr(0xA11CCC);
+
+    // act as funds admin
+    vm.startPrank(_newFundsAdmin);
+
+    // should transfer
+    collector.transfer(_token, recipient, 1000);
+
+    // should approve
+    collector.approve(_token, recipient, 1000);
+
+    // change funds admin to someone else
+    collector.setFundsAdmin(anotherFundsAdmin);
+
+    vm.stopPrank();
+
+    // act as new funds admin
+    vm.startPrank(anotherFundsAdmin);
+
+    // should transfer
+    collector.transfer(_token, recipient, 1000);
   }
 }
